f6037fb87852c5df4a65e528672c96840bf413e0
Mark A Guza <guzamaguza@gmail.com>
2020-01-05 03:27:45 +0000

    Automatically backed up by Learn

---
+++ lib/author.rb
@@ -0,0 +1,23 @@
+class Author
+    attr_accessor :name, :posts
+
+    def initialize(name)
+        @name = name
+        @posts = []
+    end
+
+    def add_post(post)
+       @posts << post  #add song to list of songs under artist name
+       post.author = self  #allow for reverse look up
+       #@@song_count +=1    #increase song count by 1
+    end
+
+    def add_post_by_title(title)
+        post = Post.new(title)
+        @posts << post 
+        post.author = self 
+    end
+    
+
+end 
+
