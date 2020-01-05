b5587bf938decb9914ca4809d9749f500aa99221
Mark A Guza <guzamaguza@gmail.com>
2020-01-05 02:52:21 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -23,7 +23,7 @@ class Artist
     end
 
     def add_song_by_name(name)
-      song = Song.new(name)  #creats a new instance of a song using the input name 
+      song = Song.new(name)  #creats a new instance of a song using the input name
       self.songs << song  #add song to list of songs under artist name
       song.artist = self  #allow for reverse look up
       @@song_count +=1    #increase song count by 1
