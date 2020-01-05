b8dfd4889025ffb37dc402a523b99242871fa58a
Mark A Guza <guzamaguza@gmail.com>
2020-01-05 03:08:55 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,5 +1,7 @@
+require 'pry'
+
 class Artist
-    attr_accessor :name
+    attr_accessor :name, :songs
 
     @@song_count = 0
 
@@ -9,22 +11,14 @@ class Artist
     end
 
     def add_song(song)
-       self.songs << song  #add song to list of songs under artist name
+       @songs << song  #add song to list of songs under artist name
        song.artist = self  #allow for reverse look up
        @@song_count +=1    #increase song count by 1
     end
 
-    def name
-      #self.name = attr_accessor :name
-    end
-
-    def songs
-      @songs
-    end
-
     def add_song_by_name(name)
       song = Song.new(name)  #creats a new instance of a song using the input name
-      self.songs << song  #add song to list of songs under artist name
+      @songs << song  #add song to list of songs under artist name
       song.artist = self  #allow for reverse look up
       @@song_count +=1    #increase song count by 1
     end
