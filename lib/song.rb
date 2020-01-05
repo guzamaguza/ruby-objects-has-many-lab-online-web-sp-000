8f80cf197bb33e2a9e7450f09b6f249168893637
Mark A Guza <guzamaguza@gmail.com>
2020-01-05 02:03:31 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,33 @@
+class Song
+    attr_accessor :artist, :name, :genre
+   
+    @@all = []
+   
+    def new(name, genre)
+      @name = name
+      @genre = genre
+      save
+    end
+   
+    def save
+      @@all << self
+    end
+   
+    def self.all
+      @@all
+    end
+
+    def name
+
+    end
+
+    def artist
+
+    end
+
+    def artist_name
+      self.artist.name
+    end
+
+end
+
