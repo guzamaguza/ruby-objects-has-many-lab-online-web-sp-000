7b36ff2b3b36461930eb1d4caf5760b351d1090b
Mark A Guza <guzamaguza@gmail.com>
2020-01-05 01:50:43 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,19 +1,22 @@
 class Artist
     attr_accessor :name
 
-    def initialize(name)
-      @name = name
-      @songs = []
+    def new(name)
+        @name = name
     end
 
-    def add_song(song)
-       @songs << song
-       song.artist = self
+    def name
+      
     end
 
     def songs
       @songs
       Song.all.select {|song| song.artist == self}
+    end
+
+    def add_song(song)
+       @songs << song
+       song.artist = self
     end
 
     def add_song_by_name(name, genre)
